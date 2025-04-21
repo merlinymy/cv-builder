8e3e4da4b1d8100029164ee6db6012ff369f832d
merlinymy <lordhorcrux@gmail.com>
2025-04-21 01:44:12 -0500

    add site icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 export function Header() {
   return (
-    <p className="text-2xl text-center m-2">ATS-Friendly Resume Builder</p>
+    <p className="text-2xl text-center m-2">ATS Friendly Resume Builder</p>
   );
 }
